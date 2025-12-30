de07214a401afee1b3d88a9559ec5932959dbfde
Luca L <lucaliautaud1@gmail.com>
2025-12-30 06:54:13 -0500

    initial build

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+// Keep this file private and do not share it with LLMs or public repos.
+const SUPABASE_CONFIG = {
+    URL: 'https://vzihhmmpaefaquonxrwv.supabase.co',
+    ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6aWhobW1wYWVmYXF1b254cnd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwNzk2MDAsImV4cCI6MjA4MjY1NTYwMH0.jfrweyDKoSKgH-u58GoX5vkc03m6B4r3x6MY0WAGKV8'
+};
